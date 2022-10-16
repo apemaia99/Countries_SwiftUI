c292fe74903b64d3f96ba2fef2db039371b754f5
Alessando Di Maio <92519597+apemaia99@users.noreply.github.com>
2022-10-16 23:34:12 +0200

    - decodable in a separate file

---
+++ Countries/Models/Country+Decodable.swift
@@ -0,0 +1,87 @@
+//
+//  Country+Decodable.swift
+//  Countries
+//
+//  Created by Alessandro Di Maio on 16/10/22.
+//
+
+import Foundation
+import CoreLocation
+
+extension Country: Decodable {
+    
+    init(from decoder: Decoder) throws {
+        
+        let root = try decoder.container(keyedBy: CodingKeys.self)
+        
+        name = try root.decode(Name.self, forKey: .name)
+        area = try root.decode(Double.self, forKey: .area)
+        population = try root.decode(Int.self, forKey: .population)
+        flags = try root.decode(Flag.self, forKey: .flags)
+        code = try root.decode(String.self, forKey: .code)
+        
+        let latlng = try root.decode([CLLocationDegrees].self, forKey: .location)
+        location = .init(latitude: latlng[0], longitude: latlng[1])
+        
+        continent = .init(
+            rawValue: try root
+                .decode([String].self, forKey: .continent)
+                .first ?? ""
+        ) ?? .none
+        
+        //MARK: - Catching errors for optionals values
+        do {
+            capital = try .init(from: decoder)
+        } catch {
+            print(Errors.missingKey("capital for \(self.name.common)"))
+        }
+        
+        do {
+            borders = try root.decode([String].self, forKey: .borders)
+        } catch {
+            print(Errors.missingKey("borders for \(self.name.common)"))
+        }
+        
+        do {
+            tlds = try root.decode([String].self, forKey: .tlds)
+        } catch {
+            print(Errors.missingKey("tld for \(self.name.common)"))
+        }
+        
+        do {
+            languages = try root
+                .decode([String : String].self, forKey: .languages)
+                .map(\.value)
+        } catch {
+            print(Errors.missingKey("languages for \(self.name.common)"))
+        }
+        
+        do {
+            let currencies = try root
+                .decode([String : Currency].self, forKey: .currencies)
+            
+            for (key, currency) in currencies {
+                self.currencies.append(
+                    Currency(name: currency.name, symbol: currency.symbol, ISO4217: key)
+                )
+            }
+        } catch {
+            print(Errors.missingKey("currencies for \(self.name.common)"))
+        }
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case continent = "continents"
+        case capital
+        case location = "latlng"
+        case borders
+        case tlds = "tld"
+        case code = "cca3"
+        case currencies
+        case languages
+        case area
+        case population
+        case flags
+    }
+}
